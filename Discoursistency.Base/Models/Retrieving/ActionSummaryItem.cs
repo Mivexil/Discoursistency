5a68a9b4537a071209bef9774dbadf0a27629dda
Mivexil <maciej.jan.stachowski@outlook.com>
2014-10-31 21:37:26 +0000

    Added a missing file.

---
+++ Discoursistency.Base/Models/Retrieving/ActionSummaryItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Discoursistency.Base.Exceptions;
+
+namespace Discoursistency.Base.Models.Retrieving
+{
+    public class ActionSummaryItem
+    {
+        public bool can_act { get; set; }
+        public bool? can_defer_flags { get; set; }
+        public int count { get; set; }
+        public bool hidden { get; set; }
+        public int id { get; set; }
+    }
+}
